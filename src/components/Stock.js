c420d2e1abe2572e4a76afb34f48737a64584813
Takako Onozuka <onozukatakako@gmail.com>
2023-03-26 17:22:08 +0100

    added border-radius on stock component

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -18,14 +18,16 @@ const { id } = useParams()
 
   const StockItem = styled.div `
 
-  display: flex;
-  flex-direction: column;
-  height: 270px;
-  width: 150px;
-  box-shadow: 2px 2px 4px grey;
-  margin: 15px;
-  background-color: white;
-  font-family: roboto;
+    display: flex;
+    flex-direction: column;
+    height: 270px;
+    width: 150px;
+    box-shadow: 2px 2px 4px grey;
+    margin: 15px;
+    background-color: white;
+  
+    border-radius: 5px 50px;
+
 `
 
   const CompanyName = styled.div `
@@ -34,13 +36,15 @@ const { id } = useParams()
     font-size: 1.5rem;
   `
   const StockDetails = styled.div `
-    padding: 0.3rem;
+    padding: 0.2rem;
     color: Snorlax;
 
   `
 
   const StockImage = styled.image`
     margin: 5px;
+    margin-left: auto;
+    margin-right: auto;
   `
 
 
@@ -59,12 +63,13 @@ return (
     <StockImage><img src={Stock_img} width="100" height="80"/></StockImage>
 
     {/* <button onClick={onClick} >If Only</button> */}
+
+            {/* <Link to="/result"> */}
+            {/* <Button text="click" /> */}
+    {/* {showResult && <IfOnlyModal state= {{ stock: stock}}/>} */}
     <Link to='/result'
            state= {{ stock: stock}}
           >
-            {/* <Link to="/result"> */}
-            {/* <Button text="click" /> */}
-    {/* {showResult && <IfOnlyModal stock={stock} name={name} currency={currency} amount={amount}/>} */}
     <button text="if only" 
         className="text-white bg-orange-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
         font-medium rounded-lg text-sm w-24 sm:w-auto px-1 py-1 text-center dark:bg-blue-600 
